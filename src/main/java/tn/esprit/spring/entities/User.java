fbd0de91ed7d5ed3befc9a4d89e559f739e17b02
kaliltebessi <mohammedkhalil.tebessi@esprit.tn>
2024-02-26 10:33:44 +0100

    ajouter commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,7 +18,7 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 
-
+	// commentaire test git
 	private static final long serialVersionUID = 1L;
 
 	@Id
